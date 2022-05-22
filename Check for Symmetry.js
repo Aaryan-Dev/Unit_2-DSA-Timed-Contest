c207ba8da4aa5e92e80f0035b4da8479d180d865
Aryan Dev <nobodygivesafak@gmail.com>
2022-05-22 23:38:12 +0530

    Good question

---
+++ Check for Symmetry.js
@@ -0,0 +1,18 @@
+// You are given a square matrix of size n. Rows are indexed 1 to n from top to bottom and columns are indexed 1 to n form left to right. Matrix consists of only '*' and '.'. You need to check whether matrix is symmetric or not. if it is, check it is symmetric about vertical axis or horizontal axis or both.
+
+// A matrix is said to be symmetric about horizontal axis if 1st row is identical to n-th row, 2nd is identical to (n-1)th row and so on.
+
+// A matrix is said to be symmetric about vertical axis if 1ST column is identical to nth column, 2nd identical to (n-1)th and so on for all columns.
+
+// Note : You don't have to take the input. 
+// Just complete the function and print the required output
+
+
+
+
+//Hint use line++;
+
+function checkForSymmetry(n, matrix) {
+    //write code here
+    //console n,matrix and check the input response
+}
